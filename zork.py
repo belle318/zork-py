da9c7ce336a0c97ce06ad1416d263c129034894d
belle318 <candybelle24@gmail.com>
2017-03-24 13:49:25 -0700

    Minor changes to while==loop 10 section

--- zork.py
+++ zork.py
@@ -142,8 +142,6 @@ while loop == 3:
 			print("You are in a tiny cave with a dark, forbidding staircase leading down.")
 			print("There is a skeleton of a human male in one corner.")
 			cave_inp = raw_input("What do you do? ")
-		if cave_inp.lower() == ("descend staircase"):
-			loop = 11
 		if cave_inp.lower() == ("take skeleton"):
 			print("---------------------------------------------------------")
 			print("Why would you do that? Are you some sort of sicko?")
@@ -160,9 +158,11 @@ while loop == 3:
 			print("---------------------------------------------------------")
 			print("I have two questions: Why and With What?")
 			loop = 10
-		if cave_inp.lower() == ("go down staircase"):
+		elif cave_inp.lower() == ("descend staircase"):
 			loop = 11
-		if cave_inp.lower() == ("scale staircase"):
+		elif cave_inp.lower() == ("go down staircase"):
+			loop = 11
+		elif cave_inp.lower() == ("scale staircase"):
 			loop = 11
 		elif cave_inp.lower() == ("suicide"):
 			print("---------------------------------------------------------")
